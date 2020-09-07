7e14fc39c5d4cc2d4968040a5a67982330f05ed5
Bernard Mordan <bernardmordan@gmail.com>
2020-09-07 12:22:03 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+
+![SWE at WhiteHat Welcome](https://user-images.githubusercontent.com/4499581/92381014-fec7f500-f101-11ea-8fcd-506232e5de87.png)
+
+## Overview
+
+1. Flying start
+1. Bootcamp
+1. Module 1
+1. Module 2
+1. Mock EPA 1
+1. Module 3
+1. Module 4
+1. Mock EPA 2
+1. Gateway
---
+++ index.js
@@ -0,0 +1,21 @@
+const unified = require('unified')
+const markdown = require('remark-parse')
+const remark2rehype = require('remark-rehype')
+const doc = require('rehype-document')
+const format = require('rehype-format')
+const html = require('rehype-stringify')
+const report = require('vfile-reporter')
+const fs = require('fs')
+const path = require('path')
+
+unified()
+  .use(markdown)
+  .use(remark2rehype)
+  .use(doc, {title: 'SWE L4 Overview'})
+  .use(format)
+  .use(html)
+  .process('# SWE L4 Overview', async function (err, file) {
+    fs.writeFile(path.join(__dirname, 'dist', 'index.html'), String(file), () => {
+      console.error(report(err || file))
+    })
+  })
